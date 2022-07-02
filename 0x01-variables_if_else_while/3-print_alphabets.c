ef6a8aa290756b69aa6579bb110a55311bc533ab
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-02 06:51:54 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,15 +14,10 @@ int main(void)
 
 		int ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-
-						putchar(ch);
-
+	putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-
-							putchar(ch);
-
-					putchar('\n');
-
-						return (0);
+		putchar(ch);
+		putchar('\n');
+		return (0);
 
 }
