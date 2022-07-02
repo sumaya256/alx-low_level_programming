450410f611d747010b5a71722134a70be3aba03c
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-02 07:27:33 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -16,18 +16,18 @@ int main(void)
 	int n;
 
 	char ch;
+	
 	for (n = 48; n < 58; n++)
-		
 {
 	     putchar(n);
 }
 	for (ch = 'a'; ch <= 'f'; ch++)
 
 {
-		putchar(ch);
+	putchar(ch);
 }
  	putchar('\n');
 
-			return (0);
+	return (0);
 
 }
