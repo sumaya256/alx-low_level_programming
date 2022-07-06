afe17e88701f5233e9e3caf0210393a5507c0436
sumaya256 <you@example.com>
2022-07-06 01:50:02 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet-prints alphabets in lower case
+ *
+ * return 0 success
+ */
+void print_alphabet(void)
+{
+char j;
+	for(j = 'a'; j <= 'z'; j++);
+{
+	_putchar(j);
+}
+	_putchar('\n');
+
+}
+
