a80c736ad63d06b020b8605ed0ee56c9c5e0944a
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-07 00:24:46 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,9 +13,9 @@ int main(void)
 	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+	printf(", %lu", k);
+	k = k + j;
+	j = k - j;
 	}
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
@@ -23,12 +23,12 @@ int main(void)
 	k2 = k % 1000000000;
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
+	printf(", %lu", k1 + (k2 / 1000000000));
+	printf("%lu", k2 % 1000000000);
+	k1 = k1 + j1;
+	j1 = k1 - j1;
+	k2 = k2 + j2;
+	j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
