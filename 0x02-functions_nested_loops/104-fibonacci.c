2172fac0063c0b978eb6d33dead6e14511c05d10
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-07 00:33:47 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - finds and prints the first 98 fibonacci numbers,
-* starting with 1 and 2
-* followed by a new line
-* Return: Always 0 (Success)
+*main - finds and prints the first 98 fibonacci numbers,
+*starting with 1 and 2
+*followed by a new line
+*Return: Always 0 (Success)
 */
 int main(void)
 {
