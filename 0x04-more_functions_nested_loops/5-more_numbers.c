c339b7431760d370901f17489c3864bfd5acf3ea
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-08 15:05:35 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,12 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+int i, j;
 	for (i = 0; i < 10; i++)
 {
-  for (j = 0; j <= 14; j++)
+	for (j = 0; j <= 14; j++)
 {
-  if (j >= 10)
+	if (j >= 10)
 	_putchar('0' + (j / 10));
 	_putchar('0' + (j % 10));
 }
