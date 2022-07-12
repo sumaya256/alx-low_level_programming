e421c7c375fc65cd959db15da16015925cb4dd64
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-13 01:41:01 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,18 @@ int _atoi(char *s)
 	int res = 0;
 	int sig = -1;
 	int brk = 0;
-  for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 {
-		if (s[i] == '-')
-			sig = sig * -1;
-		if (s[i] >= '0' && s[i] <= '9')
+	if (s[i] == '-')
+	sig = sig * -1;
+	if (s[i] >= '0' && s[i] <= '9')
 {
-			res = res * 10;
-			res -= (s[i] - '0');
-			brk = 1;
+	res = res * 10;
+	res -= (s[i] - '0');
+	brk = 1;
 }
-		else if (brk == 1)
-			break;
+	else if (brk == 1)
+	break;
 }
 	res = sig * res;
 	return (res);
