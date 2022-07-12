fb21b7e1dbe37c98204aa35fb5ec2f8f02d6786b
sumaya256 <you@example.com>
2022-07-12 14:00:11 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_rev - reversed str is printed \n
+*@s: pointer to be printed for the str
+*Return: void
+*/
+void print_rev(char *s)
+{
+int i = 0;
+	while (s[i])
+	i++;
+	while (i--)
+{
+	_putchar(s[i]);
+}
+	_putchar('\n');
+}
