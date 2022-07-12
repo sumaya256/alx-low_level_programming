a80dc101310cddc204d839c22c54a84a26c58c9d
sumaya256 <you@example.com>
2022-07-12 13:23:01 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap_int - swap variable values
+*@a: 1st pointer
+*@b: 2nd pointer
+*Return: void
+*/
+void swap_int(int *a,int *b)
+{
+	*a += *b;
+	*b=*a-*b;
+	*a= *a-*b;
+}
