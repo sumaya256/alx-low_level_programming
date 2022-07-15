cdc42dc381d98b89a458c6631a3cf42db800f883
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-16 02:33:57 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *s)
 	int i, ii;
 	char s1[] = "aeotl";
 	char S1[] = "AEOTL";
-	char s2[] = "43071";
+char s2[] = "43071";
 	for (i = 0; s[i] != '\0'; i++)
 {
 	for (ii = 0; ii < 5; ii++)
