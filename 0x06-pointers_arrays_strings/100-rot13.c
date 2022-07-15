801d7e33a70c453b99fa3e9bac3b19e9d53f77ad
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-16 02:52:39 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,14 +10,14 @@ int i, j;
 char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char boolean;
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 {
 boolean = 0;
 	for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
 	if (s[i] == alpha[j])
 {
 	s[i] = r[j];
-	boolean=1;
+	boolean = 1;
 }
 }
 }
