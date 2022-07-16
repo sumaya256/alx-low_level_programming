a3d3a4e12fe49c5c538676d9f501b0225abb19fc
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-16 03:17:21 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,12 +10,12 @@ unsigned int n1;
 n1 = n;
 	if (n < 0)
 {
-	_putchar( '_');
+	_putchar('_');
 	n1 = -n;
 }
-	if (n1 / 10 ! = 0);
+	if (n1 / 10 != 0)
 {
 	print_number(n1 / 10);
 }
-	_putchar(n1 % 10 +'0');
+	_putchar(n1 % 10 + '0');
 }
