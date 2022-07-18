a568a983f883a40a8087b8fa33da0ee474e4eb1e
sumaya256 <you@example.com>
2022-07-18 16:04:17 -0700

    task02

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_memset - fills and bytes
+*_memset - fills n bytes
 *@s: pointer to the char parameters
 *@b: data to change
 *@n: index
@@ -8,10 +8,11 @@
 */
 char *_memset(char *s, char *b, unsigned int n)
 {
-unsigned int i;
+unsigned int i
 	for (i = 0; i < n; i++)
 {
 	s[i] = b;
 }
 	return (s);
 }
+
