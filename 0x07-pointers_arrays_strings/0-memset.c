d345f54ba970872ec20f534e7ac5a068eb5d1ba9
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-19 01:34:51 +0300

    Update 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - main function
+*@s: pointer to the char parameters
+*@b: data to change
+*@n: index
+*Return: *s
+*/
+char *memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+  for (i = 0; i < n; i++)
+{
+  s[i] = b;
+}
+  return (s);
+}
+  
