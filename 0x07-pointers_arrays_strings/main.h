b44efefcbe4e8f1150a01ec8541032efa3272b32
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-19 09:01:19 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -11,4 +9,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_String(char **s, char *to);
 
-#endif
+
