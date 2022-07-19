648c4a75d02071aed03cf7704b6cbbabf4b215fc
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-20 00:11:43 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,8 +13,8 @@ char *_strpbrk(char *s, char *accept)
 {
 	while (*s != '\0')
 {
-	char *a = accept;		
-	while (*a != '\0')		
+	char *a = accept;	
+	while (*a != '\0')
 	if (*a++ == *s)
 	return ((char *)s);
 	++s;
