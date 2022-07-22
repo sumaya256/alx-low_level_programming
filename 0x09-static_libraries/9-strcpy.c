e38f1afea8353befad8f295621c8506e64d138a6
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:44:41 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,41 +1,21 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * *_strcpy - copies string pointed by src
- *
- *   * @dest: char type string
- *
- *    * @src: char type string
- *
- *     * Description: copy sting pointed by src
- *
- *      * buffer pointed to dest
- *
- *       * Return: Pointer to dest
- *
- *        */
-
+*_strcpy - copies string pointed by src
+* @dest: char type string
+*@src: char type string
+* Description: copy sting pointed by src
+* buffer pointed to dest
+* Return: Pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
-
 {
-
-	int i = -1;
-
-
-
-	do {
-
-		i++;
-
-		dest[i] = src[i];
-
-	} while (src[i] != '\0');
-
-
-
+int i = -1;
+	do
+{
+i++;
+dest[i] = src[i];
+}
+	while (src[i] != '\0');
 	return (dest);
 
-}#
+}
