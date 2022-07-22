af41d975f96a06c23c435b421b46b6e8bbc4af12
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 01:31:43 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - function
- *
- *   * @argc: offset count
- *
- *    * @argv: offset value
- *
- *     * Return: 0
- *
- *      */
-
+* main - function
+* @argc: offset count
+* @argv: offset value
+* Return: 0
+*/
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
-
-		printf("%s\n", *argv);
-
-			return (0);
-
+	printf("%s\n", *argv);
+	return (0);
 }
