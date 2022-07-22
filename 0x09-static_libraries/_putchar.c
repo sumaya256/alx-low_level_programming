78b07d7a4bb8b5358e54f8832859da62ef9bbc7e
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:40:50 +0300

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,23 +1,13 @@
-nclude <unistd.h>
-
-
-
+#include <unistd.h>
 /**
- *
- *  * _putchar - writes c character in the stdout
- *
- *   * @c: its the character to be printed
- *
- *    * Return: On success 1
- *
- *     * On error, return -1 and set error appropriately
- *
- *      */
-
+*
+* _putchar - writes c character in the stdout
+* @c: its the character to be printed
+*
+* Return: On success 1
+* On error, return -1 and set error appropriately
+*/
 int _putchar(char c)
-
 {
-
 	return (write(1, &c, 1));
-
-}#
+}
