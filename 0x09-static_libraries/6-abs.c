3a6eefd4a2524b3e2f1a4eca2d6570cea38cd1a8
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:46:26 +0300

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,21 +1,10 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * _abs - return value
- *
- *   * @n : the number to be checked
- *
- *    * Return:0 or 1
- *
- *     */
-
+* _abs - return value
+* @n : the number to be checked
+* Return:0 or 1
+ */
 int _abs(int n)
-
 {
-
 	return (n >= 0 ? n : n * -1);
-
-}#
+}
