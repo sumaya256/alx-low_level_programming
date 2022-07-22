74346ac68202023e000c23fb5f63c99e8e78cdbb
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:59:54 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,29 +1,14 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * _puts - prints str followed by a new line
- *
- *   * @str: pointer to the string to point
- *
- *    * Return: void
- *
- *     */
-
+ * _puts - prints str followed by a new line
+ * @str: pointer to the string to point
+ * Return: void
+ */
 void _puts(char *str)
-
 {
-
 	for (; *str != '\0'; str++)
-
-	{
-
-		_putchar(*str);
-
-	}
-
+{
+	_putchar(*str);
+}
 	_putchar('\n');
-
-}i#
+}
