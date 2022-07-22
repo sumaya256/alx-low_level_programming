f6084519dc8e2a77a2ae61a2b5b643255ea9a866
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:49:35 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,63 +1,29 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * _strstr - function locate
- *
- *   * @haystack: pointer to char
- *
- *    * @needle: pointer to char
- *
- *     * Return: 0
- *
- *      */
-
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
 char *_strstr(char *haystack, char *needle)
-
 {
-
 	char *result = haystack, *fneedle = needle;
-
-
-
 	while (*haystack)
-
-	{
-
-		while (*needle)
-
-		{
-
-			if (*haystack++ != *needle++)
-
-			{
-
-				break;
-
-			}
-
-		}
-
-		if (!*needle)
-
-		{
-
-			return (result);
-
-		}
-
-		needle = fneedle;
-
-		result++;
-
-		haystack = result;
-
-	}
-
-
-
+{
+	while (*needle)
+{
+	if (*haystack++ != *needle++)
+{
+	break;
+}
+}
+	if (!*needle)
+{
+	return (result);
+}
+	needle = fneedle;
+	result++;
+	haystack = result;
+}
 	return (0);
-
-}#
+}
