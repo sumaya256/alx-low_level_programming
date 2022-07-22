cf30148b174c331e26accc80bbc16a9cbf7f1b74
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 01:01:10 +0300

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,23 +1,10 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
-
- * _islower - checks for lower case letter
-
- * @c : character to check the case
-
- * Return:0 or 1
-
- */
-
+* _islower - checks for lower case letter
+* @c : character to check the case
+* Return:0 or 1
+*/
 int _islower(int c)
-
 {
-
 	return (c >= 97 && c <= 122);
-
-
-
-}#
+}
