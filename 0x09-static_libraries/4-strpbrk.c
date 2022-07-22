4d20c59af94406fda2bc44b2700aa3bb6e023ea1
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 00:52:11 +0300

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,53 +1,24 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
-
-
-
 /**
- *
- *  * _strpbrk - bytes
- *
- *   * @s: char pointer
- *
- *    * @accept: char pointer
- *
- *     * Return: Null
- *
- *      */
-
+* _strpbrk - bytes
+* @s: char pointer
+* @accept: char pointer
+* Return: Null
+*/
 char *_strpbrk(char *s, char *accept)
-
 {
-
-	int a;
-
-
-
+int a;
 	while (*s)
-
-	{
-
-		for (a = 0; accept[a]; a++)
-
-		{
-
-			if (*s == accept[a])
-
-			{
-
-				return (s);
-
-			}
-
-		}
-
-		s++;
-
-	}
-
-
-
+{
+	for (a = 0; accept[a]; a++)
+{
+	if (*s == accept[a])
+{
+	return (s);
+}
+}
+	s++;
+}
 	return (NULL);
-
-}i#
+}
