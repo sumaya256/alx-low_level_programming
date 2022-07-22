6a42a6dcc869dcd3a88a7864abb88d14dc38ef25
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 01:02:52 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,41 +1,17 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
-
- * *_strncpy - finds the length of the string
-
- * @dest: pointer to the string
-
- * @src: pointer
-
- * @n: int
-
- * Return: destination
-
- */
-
+*_strncpy - finds the length of the string
+* @dest: pointer to the string
+* @src: pointer
+* @n: int
+* Return: destination
+*/
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
-	int i;
-
-
-
+int i;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-
-		dest[i] = src[i];
-
-
-
+	dest[i] = src[i];
 	for ( ; i < n; i++)
-
-		dest[i] = '\0';
-
-
-
+	dest[i] = '\0';
 	return (dest);
-
-}#
+}
