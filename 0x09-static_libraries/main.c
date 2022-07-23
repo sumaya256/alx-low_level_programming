339d3728ea434a9a2f3298b433af5f494186a391
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-23 08:48:46 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+*_puts - function that prints the output
+*Retur: 0
+*/
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
