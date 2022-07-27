c1302c09eb0c6f1074ccc27a9f0aca76f8cd452f
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-27 22:12:33 +0300

    Update 2-main.c

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,21 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char *s;
-
+  char *s;
     s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed\n");
-        return (1);
+      printf("failed\n");
+       return (1);
     }
     printf("%s\n", s);
     free(s);
