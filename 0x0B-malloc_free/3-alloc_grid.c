a2c9ace01121f4ac933e3e19411111c3055940b4
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-27 22:29:50 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,87 +1,46 @@
-#include "main.h"
-
-
-
+#include <stdlib.h>
 /**
- *
- *  * alloc_grid - returns a pointer to a 2D array of integers
- *
- *   * @width: width oàf array
- *
- *    * @height: height of array
- *
- *     * Return: pointer to the allocated grid
- *
- *      */
-
+* alloc_grid - Create a 2 dimensional array
+* @width: Width of grid
+* @height: Height of grid
+*
+* Return: Pointer to 2D array, NULL if fails
+*/
 int **alloc_grid(int width, int height)
-
 {
-
-		int **array;
-
-			int i = 0, j;
-
-
-
-				if (width == 0 || height == 0)
-
-							return (NULL);
-
-					array = (int **) malloc(sizeof(int *) * height);
-
-						if (array != NULL)
-
-								{
-
-											for (; i < height; i++)
-
-														{
-
-																		array[i] = (int *) malloc(sizeof(int) * width);
-
-																					if (array[i] != NULL)
-
-																									{
-
-																														for (j = 0; j < width; j++)
-
-																																				array[i][j] = 0;
-
-																																	}
-
-																								else
-
-																												{
-
-																																	while (i >= 0)
-
-																																						{
-
-																																												free(array[i]);
-
-																																																	i--;
-
-																																																					}
-
-																																					free(array);
-
-																																									return (NULL);
-
-																																												}
-
-																										}
-
-													return (array);
-
-														}
-
-							else
-
-									{
-
-												return (NULL);
-
-													}
-
+	int **grid;
+	int i, j;
+	i = 0;
+	if (width <= 0 || height <= 0)
+	return (NULL);
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
+{
+	free(grid);
+	return (NULL);
+}
+	while (i < height)
+{
+	grid[i] = malloc(width * sizeof(**grid));
+	if (grid[i] == NULL)
+{
+	i--;
+	while (i >= 0)
+{
+	free(grid[i]);
+	i--;
+}
+	free(grid);
+	return (NULL);
+}
+	j = 0;
+	while (j < width)
+{
+	grid[i][j] = 0;
+	j++;
+}
+	i++;
+}
+	i = 0;
+	return (grid);
 }
