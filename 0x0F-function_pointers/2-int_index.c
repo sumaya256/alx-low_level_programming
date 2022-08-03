f8ee4c80630dc7ae5d8b4e9da38d8036899f1bca
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-04 00:05:00 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,71 +1,31 @@
 #include "function_pointers.h"
-
 #include <stdlib.h>
-
-
-
 /**
- *
- *  * int_index - searches for an integer
- *
- *   *
- *
- *    * @array: array to search
- *
- *     * @size: size of the array
- *
- *      * @cmp: pointer to function that checks for the integer
- *
- *       * Return: -1 if no integer or if integer is <= 0, or index of
- *
- *        * integer if int is > 0
- *
- *         */
-
-
-
+* int_index - searches for an integer
+* @array: array to search
+* @size: size of the array
+* @cmp: pointer to function that checks for the integer
+* Return: -1 if no integer or if integer is <= 0, or index of
+* integer if int is > 0
+*/
 int int_index(int *array, int size, int (*cmp)(int))
-
 {
-
-		int i, check;
-
-
-
-			if (size <= 1 || cmp == NULL || array == NULL)
-
-					{
-
-								return (-1);
-
-									}
-
-
-
-				for (i = 0; i < size; i++)
-
-						{
-
-									check = cmp(array[i]);
-
-											if (check != 0)
-
-														{
-
-																		break;
-
-																				}
-
-												}
-
-					if (check == 0)
-
-							{
-
-										return (-1);
-
-											}
-
-						return (i);
-
+int i, check;
+	if (size <= 1 || cmp == NULL || array == NULL)
+{
+	return (-1);
+}
+	for (i = 0; i < size; i++)
+{
+	check = cmp(array[i]);
+	if (check != 0)
+{
+	break;
+}
+}
+	if (check == 0)
+{
+	return (-1);
+}
+	return (i);
 }
