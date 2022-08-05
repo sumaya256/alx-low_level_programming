9d8e740cf72aaf1c143c7a435408c8c2ffad4d5a
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-05 13:33:20 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
-int putchar(c);
+int putchar(int c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
