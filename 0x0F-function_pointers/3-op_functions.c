2803134cca5491f9686274cf10617f484eda2ebd
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-05 12:58:49 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,51 +1,62 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* op_add - returns the sum of a and b
-* @a: first integer
-* @b: second integer
-* Return: sum of a and b
+* op_add - addition operator
+* @a: first int
+* @b: second int
+* Return: sum of 2 numbers
 */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 /**
-* op_sub - subtracts two numbers
-* @a - first number
-* @b - second number
-* Return: difference of a and b
+* op_sub - subtraction operator
+* @a: first in
+* @b: second int
+* Return: difference of 2 numbers
 */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 /**
-* op_mul - multiplies two numbers
-* @a: first number
-* @b: second number
-* Return: product of a and b
+* op_mul - product of 2 numbers
+* @a: first int
+* @b: second int
+* Return: product of the numbers
 */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
-* op_div - divides two numbers
-* @a: firsnt number
-* @b: second number
-* Return: result of division
+* op_div - division of 2 numbers
+* @a: first int
+* @b: second int
+* Return: result of the division of the numbers
 */
 int op_div(int a, int b)
 {
+	if (b == 0)
+{
+	printf("Error\n");
+	exit(100);
+}
 	return (a / b);
 }
 /**
-* op_mod - calculates the remainder of the division of two numbers
-* @a: first number
-* @b: second number
-* Return: remainder of division
+* op_mod - get remainder of the division of 2 numbers
+* @a: first int
+* @b: second int
+* Return: remainder of division of the numbers
 */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+{
+	printf("Error\n");
+	exit(100);
+}
 	return (a % b);
 }
