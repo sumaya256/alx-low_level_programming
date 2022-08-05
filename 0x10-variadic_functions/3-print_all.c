1caa8b09edecfb98ebc8ad7bbe1f5be2bce7e046
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-05 13:49:51 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,19 @@
 #include "variadic_function.h"
-
 #include <stdarg.h>
-
 #include <stdio.h>
-
-
-
 /**
- *
- *  * c - prints character
- *
- *   */
-
-
-
+* c - prints character
+*/
 char c(char a)
-
 {
-
-		if (a)
-
-					printf("%c", a);
-
+	if (a)
+	printf("%c", a);
 }
-
 /**
- *
- *  * print_all - prints anything
- *
- *   *
- *
- *    * @format: format of the string to be printed
- *
- *     */
-
-
-
+* print_all - prints anything
+* @format: format of the string to be printed
+*/
 void print_all(const char * const format, ...)
-
 {
-
-		va_list
-
+	va_list
 }
