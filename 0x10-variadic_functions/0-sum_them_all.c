44de7f3a4af86d107a933b7e336eadd57d1265b7
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-05 13:38:17 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,10 +7,10 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum = 0;
-	va_list ap;
-	va_start(ap, n);
+unsigned int i;
+int sum = 0;
+va_list ap;
+va_start(ap, n);
 	for (i = 0; i < n; i++)
 {
 	sum += va_arg(ap, unsigned int);
