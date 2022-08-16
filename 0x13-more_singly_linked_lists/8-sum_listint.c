d530d04f885f529dbff675fce5522529cfbd9a43
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-16 12:16:04 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,49 +1,22 @@
 #include "lists.h"
-
-
-
 /**
- *
- *  * sum_listint - adds all the data in a list
- *
- *   * @head: pointer to the first element of the list
- *
- *    * Return: sum or NULL if list is empty
- *
- *     */
-
-
-
+* sum_listint - adds all the data in a list
+* @head: pointer to the first element of the list
+* Return: sum or NULL if list is empty
+*/
 int sum_listint(listint_t *head)
-
 {
-
-		listint_t *temp;
-
-			int sum = 0;
-
-
-
-				temp = head;
-
-					if (head == NULL)
-
-							{
-
-										return (0);
-
-											}
-
-						while (temp)
-
-								{
-
-											sum += temp->n;
-
-													temp = temp->next;
-
-														}
-
-							return (sum);
-
+listint_t *temp;
+int sum = 0;
+temp = head;
+	if (head == NULL)
+{
+	return (0);
+}
+	while (temp)
+{
+	sum += temp->n;
+	temp = temp->next;
+}
+	return (sum);
 }
