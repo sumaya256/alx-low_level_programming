0b37dce076d9463a49cfa1b4538216566aa36c3f
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-16 12:06:15 +0300

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,39 +1,16 @@
 #include "lists.h"
-
 #include <stdlib.h>
-
-
-
 int pop_listint(listint_t **head)
-
 {
-
-		listint_t *temp;
-
-			int td;
-
-
-
-				temp = *head;
-
-
-
-					if (*head == NULL)
-
-							{
-
-										return (0);
-
-											}
-
-						temp = temp->next;
-
-							td = *head ->n;
-
-								free(*head);
-
-
-
-									return (td);
-
+listint_t *temp;
+int td;
+temp = *head;
+	if (*head == NULL)
+{
+	return (0);
+}
+	temp = temp->next;
+	td = *head ->n;
+	free(*head);
+	return (td);
 }
