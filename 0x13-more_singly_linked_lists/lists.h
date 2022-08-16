55655e59174b56dc73f364e39216aa4080efb480
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-16 11:45:29 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,9 +9,10 @@
 */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+  int n;
+  struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -23,4 +24,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
