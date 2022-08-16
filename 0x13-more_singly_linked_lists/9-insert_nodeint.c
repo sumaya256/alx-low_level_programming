d17ef32d7c3c13f4ba87043ce346f8067067c05c
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-08-16 12:21:49 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,99 +1,45 @@
 #include "lists.h"
-
 #include <stdlib.h>
-
-
-
 /**
- *
- *  * insert_nodeint_at_index - inserts a new node at a given position
- *
- *   * @head: pointer to pointer to the first element
- *
- *    * @idx: index where new element should be added
- *
- *     * @n: element that should be added.
- *
- *      * Return: address of new node or NULL
- *
- *       */
-
-
-
+* insert_nodeint_at_index - inserts a new node at a given position
+* @head: pointer to pointer to the first element
+* @idx: index where new element should be added
+* @n: element that should be added.
+* Return: address of new node or NULL
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-
 {
+listint_t *newnode, *temp;
+unsigned int a = 0, count = 0;
+/* assign memory for new node element */
+newnode = malloc(sizeof(listint_t));
+	if (!newnode)
+{
+	return (NULL);
+}
+	newnode->n = n;
+/* count number of elements in list */
+	temp = *head;
+	while (temp)
+{
+count++;
+temp = temp->next;
+}
+/* check if index is greater than no. of elements */
+	if (a > count)
+{
+	return (NULL);
 
-		listint_t *newnode, *temp;
-
-			unsigned int a = 0, count = 0;
-
-
-
-				/* assign memory for new node element */
-
-				newnode = malloc(sizeof(listint_t));
-
-
-
-					if (!newnode)
-
-							{
-
-										return (NULL);
-
-											}
-
-						newnode->n = n;
-
-
-
-							/* count number of elements in list */
-
-							temp = *head;
-
-								while (temp)
-
-										{
-
-													count++;
-
-															temp = temp->next;
-
-																}
-
-
-
-									/* check if index is greater than no. of elements */
-
-									if (a > count)
-
-											{
-
-														return (NULL);
-
-															}
-
-										else
-
-												{
-
-															temp = *head;
-
-																	while (a < idx) /* iterate to idx and insert new node */
-
-																				{
-
-																								temp = temp->next;
-
-																											a++;
-
-																													}
-
-																			temp = newnode;
-
-																					return (temp);
-
-																						}
-
+}
+	else
+{
+	temp = *head;
+	while (a < idx) /* iterate to idx and insert new node */
+{
+	temp = temp->next;
+	a++;
+}
+	temp = newnode;
+	return (temp);
+}
 }
